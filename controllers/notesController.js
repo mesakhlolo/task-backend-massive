6af2f67fc6ae56f7ea0326fe0aa9faa2fcab4ad9
Mesakh Lolo <91011194+mesakhlolo@users.noreply.github.com>
2024-12-03 17:13:42 +0700

    Add notes controller and routes for CRUD operations

---
+++ controllers/notesController.js
@@ -0,0 +1,16 @@
+import db from "../database.js";
+
+// Get all notes
+export const getAllNotes = async (req, res) => {};
+
+// Get a single note by ID
+export const getNoteById = async (req, res) => {};
+
+// Create a new note
+export const createNote = async (req, res) => {};
+
+// Update a note
+export const updateNote = async (req, res) => {};
+
+// Delete a note
+export const deleteNote = async (req, res) => {};
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import notesRouter from "./routes/notes.js";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+// notes router
+app.use("/notes", notesRouter);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
